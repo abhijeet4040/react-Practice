fe80fb93c1027a22edfa567087aca7000a0ee2fd
abhijeet <abhijeet.k@binmile.com>
2023-08-29 11:50:32 +0530

    functional component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import Greet from './components/Greet';
 
 function App() {
   return (
     <div className="App">
-      
+     <Greet />
     </div>
   );
 }
---
+++ src/components/Greet.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Greet : React.FC = ()=><h1>hello Abhijeet</h1>;
+
+export default Greet;
